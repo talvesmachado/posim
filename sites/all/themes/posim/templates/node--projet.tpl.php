0077a847a0942d0fb4d90467822f597bd6e4da73
talvesmachado <talvesmachado@pixelis.fr>
2013-02-10 11:03:14 +0100

    caption upd

--- sites/all/themes/posim/templates/node--projet.tpl.php
+++ sites/all/themes/posim/templates/node--projet.tpl.php
@@ -121,7 +121,11 @@
             );
     
         ?>
-        <div class="slide"><a href="<?php print  file_create_url($value['uri']);?>" target="_blank"><?php	print theme_image_style($imgArray);?></a><div class="caption"><p><?php print $value['title']; ?></p></div></div>
+        <div class="slide"><a href="<?php print  file_create_url($value['uri']);?>" target="_blank"><?php	print theme_image_style($imgArray);?></a>
+        	<?php if($value['title']): ?>
+        	<div class="caption"><p><?php print $value['title']; ?></p></div>
+        	<?php endif; ?>
+        	</div>
         <?php
         };
         ?>
